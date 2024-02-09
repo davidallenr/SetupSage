22161f4bad9342e3d938db8b76e82c2f33e5036e
David <566956+davidallenr@users.noreply.github.com>
2024-02-09 18:57:47 -0500

    fixing photos script

--- _scripts/setup-mountsPhotos_ubuntu.sh
+++ _scripts/setup-mountsPhotos_ubuntu.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-# Create SD directories
-sudo mkdir -p /mnt/tv
-sudo mkdir -p /mnt/movies
-
-# Create 4k directories
-sudo mkdir -p /mnt/tv_4k
-sudo mkdir -p /mnt/movies_4k
-
+# Create photos directories
+sudo mkdir -p /mnt/photos
 
 # Check if nfs-common is already installed
 if ! dpkg -l | grep -qw nfs-common; then
@@ -18,12 +12,7 @@ else
 fi
 
 # Add entries to /etc/fstab
-echo "192.168.10.11:/mnt/media_server/media/movies /mnt/movies nfs defaults,user,exec 0 0" | sudo tee -a /etc/fstab
-echo "192.168.10.11:/mnt/media_server/media/tv /mnt/tv nfs defaults,user,exec 0 0" | sudo tee -a /etc/fstab
-
-# 4k mounts
-echo "192.168.10.11:/mnt/media_server/media/movies_4k /mnt/movies_4k nfs defaults,user,exec 0 0" | sudo tee -a /etc/fstab
-echo "192.168.10.11:/mnt/media_server/media/tv_4k /mnt/tv_4k nfs defaults,user,exec 0 0" | sudo tee -a /etc/fstab
+echo "192.168.10.11:/mnt/media_server/media/photos /mnt/photos nfs defaults,user,exec 0 0" | sudo tee -a /etc/fstab
 
 # Mount everything
 sudo mount -a
